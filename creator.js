37f30dbabdcba8cfd99f8df3ab46a52ba4b9a0f8
Logan Pritchett <logan.pritchett01@gmail.com>
2025-01-17 22:11:07 -0600

    chore: initial commit

---
+++ creator.js
@@ -0,0 +1,17 @@
+// Get all mp4 files in directory (to determine later)
+
+// For each mp4 file, find its corresponding json file
+
+// In the json file, find the "url" field
+
+// Parse the url to get the creator name
+
+// See if the creator name folder exists (in separate directory to determine later)
+
+// If it doesn't, create it
+
+// Move the mp4 file into the creator name folder
+
+// Delete the json file
+
+// Repeat for all mp4 files
